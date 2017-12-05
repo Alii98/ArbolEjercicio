a5b4d7cc42e2d5a8771766bafc18a4a0ef1a2ea1
Alexis <alexis>
2017-12-05 17:59:21 -0600

    Agrege los métodos de mayor, menor y búscar nodo

--- src/sample/ArbolBinario.java
+++ src/sample/ArbolBinario.java
@@ -1,5 +1,41 @@
 package sample;
 public class ArbolBinario {
+    public Nodo raiz;
+    public String buscarNodo(int valor) {
+        Nodo aux = raiz;
+        while (aux.getValor( ) != valor) {
+            if (valor < aux.getValor( )) {
+                aux = aux.nodoIzquierdo;
+            } else {
+                aux = aux.nodoDerecho;
+            }
+            if (aux == null) {
+                return valor+" no fue encontado";
+            }
+        }
+        return valor+ " fue encontrado";
+    }
 
 
+    public String menorValor() {
+        if (raiz != null) {
+            Nodo reco = raiz;
+            while (reco.getNodoIzquierdo() != null) {
+                reco = reco.getNodoIzquierdo();
+            }
+            return "Menor valor del árbol:" + reco.getValor();
+        }
+        return "El arbol no tiene valores";
+    }
+    public String mayorValor() {
+        if (raiz != null) {
+            Nodo reco = raiz;
+            while (reco.getNodoDerecho() != null) {
+                reco = reco.getNodoDerecho();
+            }
+            return "Mayor valor del árbol:" + reco.getValor();
+        }
+        return "El arbol no tiene valores";
+    }
+    public boolean vacia(){return raiz==null;}
 }
