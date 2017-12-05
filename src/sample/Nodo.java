8b53a691d99e07996734145e4a151d5c8692ad97
yuli <ym8857049@gmail.com>
2017-12-05 12:54:02 -0600

    Agrege Clase Nodo

--- src/sample/Nodo.java
+++ src/sample/Nodo.java
@@ -3,6 +3,22 @@ package sample;
 
 public class Nodo {
 
+    public int valor;//Valor que da el usuario al elemneto
+    public Nodo nodoIzquierdo,nodoDerecho;//Apunta al siguiente
+    public Nodo(int valor) {this.valor=valor;this.nodoDerecho=null;this.nodoIzquierdo=null;}
+    public int getValor() {return valor;}
+    public Nodo getNodoIzquierdo() {return nodoIzquierdo;}
+    public Nodo getNodoDerecho() {
+        return nodoDerecho;
+    }
+    public void setNodoIzquierdo(Nodo nodoIzquierdo) {
+        this.nodoIzquierdo = nodoIzquierdo;
+    }
+    public void setNodoDerecho(Nodo nodoDerecho) {
+        this.nodoDerecho = nodoDerecho;
+    }
+
+
 
 
 }
