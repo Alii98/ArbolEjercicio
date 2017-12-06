04b98adbcfbbd061f27578065288ac061b0190ea
yuli <ym8857049@gmail.com>
2017-12-06 11:44:54 -0600

    Agrege el metodo contar

--- src/sample/ArbolBinario.java
+++ src/sample/ArbolBinario.java
@@ -1,5 +1,35 @@
-package sample;
+﻿package sample;
 public class ArbolBinario {
 
+ public int tamaño;
+    public ArbolBinario() {
 
+        raiz=null;
+    }
+ public void agregarNodo(int valor){
+        tamaño++;
+        Nodo nuevo=new Nodo(valor);
+        if(raiz==null){
+            raiz=nuevo;
+        }else {
+            Nodo auxiliar=raiz;
+            Nodo padre;
+            while (true){
+                padre=auxiliar;
+                if(valor<auxiliar.valor){
+                    auxiliar=auxiliar.nodoIzquierdo;
+                    if(auxiliar==null){
+                        padre.nodoIzquierdo=nuevo;
+                        return;
+                    }
+                }else {
+                    auxiliar = auxiliar.nodoDerecho;
+                    if(auxiliar==null){
+                        padre.nodoDerecho=nuevo;
+                        return;
+                    }
+                }
+ controllerArbolBinario c=new controllerArbolBinario();
+    public String getTamaño(){return String.valueOf(tamaño);}
+            
 }
--- src/sample/controllerArbolBinario.java
+++ src/sample/controllerArbolBinario.java
@@ -10,20 +10,18 @@ import javafx.scene.layout.Pane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-/**
- * Created by Alicia Flores on 28/11/2017.
- */
 public class controllerArbolBinario implements Initializable {
-    @FXML
-    Button PreOrdenBtn, InOrdenBtn, PostOrdenBtn,ContarBtn, NodoMinBtn, NodoMaxBtn, BuscarBtn;
-    @FXML
-    Label LabelLbl, TextoLb;
-    @FXML
-    Pane panel;
-    @FXML
-    TextField buscar;
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
 
+
+
+        this.ContarBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                LabelLbl.setText("");
+                LabelLbl.setText("Tamaño:");
+                TextoLb.setText(arbolBinario.getTamaño());
+});
+
+            }
     }
 }
